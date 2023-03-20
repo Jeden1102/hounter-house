0fa2e89e0cb005382b6a92af831052d61008acfb
Jeden1102 <bum123@o2.pl>
2023-03-20 22:51:35 +0100

    ui-components. Button finished + badge.

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -1,22 +1,28 @@
 <template>
   <a
-    :class="['base-btn', variant]"
+    :class="['base-btn', variant, { 'icon-first': iconFirst }]"
     v-if="url"
     :type="type"
     :href="url"
     target="_blank"
   >
-    <slot></slot>
+    <font-awesome-icon :icon="icon" />
+    <span><slot></slot></span>
   </a>
   <router-link
-    :class="['base-btn', variant]"
+    :class="['base-btn', variant, { 'icon-first': iconFirst }]"
     v-if="component"
     :to="{ name: component }"
   >
-    <slot></slot>
+    <font-awesome-icon :icon="icon" />
+    <span><slot></slot></span>
   </router-link>
-  <button :class="['base-btn', variant]" v-if="!url && !component">
-    <slot></slot>
+  <button
+    :class="['base-btn', variant, { 'icon-first': iconFirst }]"
+    v-if="!url && !component"
+  >
+    <font-awesome-icon :icon="icon" />
+    <span><slot></slot></span>
   </button>
 </template>
 
@@ -30,16 +36,56 @@ const props = defineProps<{
   url?: string;
   type?: string;
   component?: string;
+  iconFirst?: boolean;
 }>();
 </script>
 
 <style scoped lang="scss">
 .base-btn {
   all: unset;
-  padding: 10px 20px;
+  padding: 12px 24px;
+  gap: 8px;
   border-radius: 32px;
-  background: #d1fae5;
-  color: #047857;
-  font-weight: bold;
+  display: flex;
+  width: fit-content;
+  cursor: pointer;
+  opacity: 0.95;
+  transition: 0.2s;
+  flex-direction: row-reverse;
+  &.icon-first {
+    flex-direction: row;
+  }
+  svg {
+    width: 20px;
+    height: 20px;
+  }
+  &:hover {
+    opacity: 1;
+  }
+  &.success-light {
+    background: $light-green;
+    color: $secondary-green;
+  }
+  &.success {
+    background: $primary-green;
+    color: white;
+  }
+  &.base {
+    background: white;
+    color: #888b97;
+  }
+  &.transparent {
+    background: rgba(255, 255, 255, 0.1);
+    border: 1px solid rgba(255, 255, 255, 0.3);
+    color: white;
+  }
+  &.primary {
+    color: #1d4ed8;
+    background: #dbeafe;
+  }
+  &.error {
+    background: #fee2e2;
+    color: #ef4444;
+  }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    <BaseButton :url="'https://google/com'" :variant="'primary'"
+    <BaseButton
+      :url="'https://google/com'"
+      :variant="'primary'"
+      :icon="'fa-solid fa-house'"
+      :iconFirst="true"
       >Outer Link</BaseButton
     >
     <BaseButton :component="'about'" :variant="'success'"
@@ -8,9 +12,11 @@
     >
     <BaseButton :component="'about'" :variant="'error'">Inner Link</BaseButton>
     <BaseButton :variant="'transparent'">Just a button</BaseButton>
+    <BaseBadge :variant="'primary'">Test Badge</BaseBadge>
   </div>
 </template>
 
 <script setup lang="ts">
 import BaseButton from "../components/ui/BaseButton.vue";
+import BaseBadge from "../components/ui/BaseBadge.vue";
 </script>
